a569a1947befbb53bd3d53c6d3da5bb29c3a4abd
Gautier Miquet <gautier.miquet@isen.yncrea.fr>
2019-11-05 08:33:17 +0100

    begin VarManager

--- src/VarManager.cpp
+++ src/VarManager.cpp
@@ -1,3 +1,33 @@
 #include "VarManager.h"
 
 VarManager::VarManager(bool expandable, int size) : memory(new Memory(expandable, size)){};
+
+int VarManager::set_double(string name, double value)
+{
+
+    bitset<64> b = double_to_bin(value);
+    vector<bitset<BYTE_SIZE>> blocks;
+
+    for (int i = 0; i < 8; i++)
+    {
+        blocks.push_back(bitset<BYTE_SIZE>());
+
+        for (int j = 0; j < 8; j++)
+        {
+            blocks[i][j] = b[(7 - i) * 8 + j];
+        }
+    }
+
+    int addr = memory.affect(blocks);
+
+    if (addr != -1)
+        linker[name] = addr;
+
+    return addr;
+}
+
+Memory &VarManager::get_mem()
+{
+
+    return memory;
+}
--- src/VarManager.h
+++ src/VarManager.h
@@ -1,55 +1,38 @@
 #ifndef GALENACPP_VARMANAGER_CLASS
 #define GALENACPP_VARMANAGER_CLASS
 
+#include <iostream>
+#include <iomanip>
+
+#include <map>
+#include <string>
+
 #include "./Memory.h"
+#include "./Binary.h"
+
+using namespace std;
+using namespace Binary;
 
 class VarManager
 {
 public:
     VarManager(bool expandable = true, int size = 0);
 
-    template <typename T>
-    int set_var(string name, T value);
+    int set_double(string name, double value);
+    // int set_int(string name, int value);
 
-    // Memory &get_mem();
+    // double get_double(string name);
+    // int get_int(string name);
+
+    // void unset(string name);
+
+    Memory &get_mem();
+
+    friend ostream &operator<<(ostream &os, VarManager &vm);
 
 private:
     Memory memory;
+    map<string, int> linker;
 };
-
-template <typename T>
-int VarManager::set_var(string name, T value)
-{
-    // case type string, case type char*, others
-    if (typeid(T) == typeid(string))
-        return -1; // TODO: Code for string
-    if (typeid(T) == typeid(char *))
-        return -1; // TODO: Code for char*
-
-    int size = sizeof(value);
-
-    auto bits = bitset<sizeof(value) * 8>(value);
-    vector<bitset<8>> blocks;
-
-    cout << bits << endl;
-
-    for (int i = 0; i < size; i++)
-    {
-        bitset<8> block;
-
-        for (int j = 0; j < 8; j++)
-        {
-            block[j] = bits[j + 8 * i];
-        }
-
-        blocks.push_back(block);
-    }
-
-    int addr = memory.affect(blocks);
-
-    cout << memory << endl;
-
-    return addr;
-}
 
 #endif // !GALENACPP_VARMANAGER_CLASS
